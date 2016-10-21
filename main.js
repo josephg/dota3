9a65d749af41485681e30f6e6ec91d0c2a42bb97
Joseph Gentle <me@josephg.com>
2016-10-22 10:03:13 +1100

    Added creeps

--- main.js
+++ main.js
@@ -42,6 +42,8 @@ class Entity {
     if (this.components[c.name]) throw Error('already compnent called ' + c.name)
     this.components[c.name] = c
   }
+
+  get c() { return this.components }
 }
 
 
@@ -70,10 +72,56 @@ e.components['yo'] = {
 world.entities.add(e)
 
 
-// entities
-// components {}
-// draw() -> draw on components
-// update() -> update on components
+class CreepComponent {
+  constructor(e, side) {
+    this.side = side
+  }
+  update(e, dt) {
+    e.c.body.y += this.side * 50 * dt
+  }
+  draw(e) {
+    ctx.fillStyle = (this.side == 1 ? "green" : "red")
+    const {x,y} = e.components.body
+    ctx.beginPath()
+    ctx.arc(x, y, 5, 0, Math.PI*2)
+    ctx.fill()
+  }
+  get name() { return 'creep' }
+}
+
+
+const spawnCreep = (side) => {
+  let e = new Entity
+  e.addComponent(new BodyComponent(e))
+  e.addComponent(new CreepComponent(e, side))
+  if (side === -1) {
+    e.c.body.y = 1000
+  }
+  e.c.body.x = 100 + (Math.random() * 2 - 1) * 10
+  return e
+}
+
+
+const makeSpawner = (side) => {
+  let spawner = new Entity
+  spawner.c['spawn'] = {
+    get name() { return 'spawn' },
+    cooldown: 1,
+    update(e, dt) {
+      this.cooldown -= dt
+      if (this.cooldown <= 0) {
+        this.cooldown += 1
+        let creep = spawnCreep(side)
+        world.entities.add(creep)
+      }
+    }
+  }
+  return spawner
+}
+
+world.entities.add(makeSpawner(1))
+world.entities.add(makeSpawner(-1))
+
 
 
 
@@ -87,7 +135,7 @@ ctx.scale(devicePixelRatio, devicePixelRatio) // For high dpi display support
 
 
 function frame() {
-  ctx.fillStyle = 'skyblue'
+  ctx.fillStyle = '#8EC401'
   ctx.fillRect(0, 0, width, height)
 
   world.update(1/60)
