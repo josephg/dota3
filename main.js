fcd385d0330f3ffd6aa1455b39f84547f4d74c62
Jeremy Apthorp <nornagon@nornagon.net>
2016-10-22 10:33:35 +1100

    shooting

--- main.js
+++ main.js
@@ -17,6 +17,21 @@ const world = {
     this.entities.forEach(e => e.draw())
   },
   entities: new Set(),
+  entitiesInCircle({x, y}, r) {
+    let es = []
+    this.entities.forEach(e => {
+      if (e.c.body) {
+        let {x: ox, y: oy, radius: or} = e.c.body
+        let dx = ox-x
+        let dy = oy-y
+        let sumr = r + or
+        if (dx*dx + dy*dy <= sumr*sumr) {
+          es.push(e)
+        }
+      }
+    })
+    return es
+  },
 }
 
 
@@ -44,6 +59,8 @@ class Entity {
   }
 
   get c() { return this.components }
+
+  get isAlive() { return world.entities.has(this) }
 }
 
 
@@ -51,42 +68,74 @@ class BodyComponent {
   constructor(e) {
     this.x = 0
     this.y = 0
+    this.radius = 5
   }
   get name() { return 'body' }
 }
 
 
-const e = new Entity()
-e.addComponent(new BodyComponent(e))
-
-e.components['yo'] = {
-  draw(e) {
-    ctx.fillStyle = 'red'
-    const {x,y} = e.components.body
-    ctx.fillRect(x-5, y-5, 10, 10)
-  },
-  update(e, dt) {
-    e.components.body.y += dt * 100
+class AlignmentComponent {
+  constructor(e, side) {
+    this.side = side
   }
+  get name() { return 'align' }
 }
-world.entities.add(e)
-
 
 class CreepComponent {
   constructor(e, side) {
     this.side = side
+    this.target = null
   }
   update(e, dt) {
-    e.c.body.y += this.side * 50 * dt
+    if (this.target && !this.target.isAlive) this.target = null
+    if (this.target == null) {
+      let es = world.entitiesInCircle(e.c.body, 100)
+        .filter(other => other.c.align && other.c.align.side != e.c.align.side)
+      if (es.length) {
+        this.target = es[0]
+      }
+    }
+    if (this.target != null) {
+      let dx = e.c.body.x - this.target.c.body.x
+      if (Math.abs(dx) > 4) {
+        e.c.body.x += -Math.sign(dx) * 50 * dt
+      } else {
+        world.entities.add(makeBullet(e.c.body, this.side))
+      }
+    } else {
+      e.c.body.y += this.side * 50 * dt
+    }
   }
   draw(e) {
     ctx.fillStyle = (this.side == 1 ? "green" : "red")
     const {x,y} = e.components.body
     ctx.beginPath()
-    ctx.arc(x, y, 5, 0, Math.PI*2)
+    ctx.arc(x, y, e.c.body.radius, 0, Math.PI*2)
     ctx.fill()
   }
   get name() { return 'creep' }
+}
+
+class BulletComponent {
+  get name() { return 'bullet' }
+  constructor(e, side) {
+    this.side = side
+  }
+
+  draw(e) {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(e.c.body.x - 2, e.c.body.y - 2, 4, 4)
+  }
+
+  update(e, dt) {
+    e.c.body.y += this.side * 100 * dt
+    let nearby = world.entitiesInCircle(e.c.body, 2)
+      .filter(other => other.c.align && other.c.align.side != this.side)
+    if (nearby.length) {
+      world.entities.delete(e)
+      world.entities.delete(nearby[0])
+    }
+  }
 }
 
 
@@ -94,10 +143,20 @@ const spawnCreep = (side) => {
   let e = new Entity
   e.addComponent(new BodyComponent(e))
   e.addComponent(new CreepComponent(e, side))
+  e.addComponent(new AlignmentComponent(e, side))
   if (side === -1) {
     e.c.body.y = 1000
   }
   e.c.body.x = 100 + (Math.random() * 2 - 1) * 10
+  return e
+}
+
+const makeBullet = ({x, y}, side) => {
+  let e = new Entity
+  e.addComponent(new BodyComponent(e))
+  e.addComponent(new BulletComponent(e, side))
+  e.c.body.x = x
+  e.c.body.y = y
   return e
 }
 
